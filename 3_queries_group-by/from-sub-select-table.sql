1c88438c86f42e2f51e67a4eabff8dca3840cc06
Jonathan Phair <jmphair@gmail.com>
2023-02-06 17:43:43 -0500

    add example sql files for search/column/from

---
+++ 3_queries_group-by/from-sub-select-table.sql
@@ -0,0 +1,13 @@
+SELECT avg(total_students) as average_students
+FROM (
+  SELECT count(students) as total_students, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+) as totals_table;
+
+-- SELECT * FROM (
+--   SELECT something_id
+--   FROM someTable
+--   WHERE something
+-- ) as sub_table;
